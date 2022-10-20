15e29103ba63c25e4ab9cd4e0f37181e2cfc45f7
matthieu-chevalier <matthieu.chevalier.pro@gmail.com>
2022-10-20 10:41:38 +0200

    init

--- index.ts
+++ index.ts
@@ -1,40 +1,11 @@
-import { meals } from "./db";
-import { mealType } from "./mealType";
 
-// Création du TYPE 'Product' contenant des PROPRIETES
-export type Product = {
-  idIngredient?: string;
-  strIngredient: string;
-  strDescription?: string | null;
-  strType?: string | null;
-  price?: number;
-  stock?: number;
-};
-
-// Initialisation du TABLEAU 'products' qui accueillera les données de meals
-// let meals: Product[] =[];
-
-console.log("Avant:" + meals.length);
-
-function addProduct(data: Product) {
-  meals.push(data);
-}
-
-addProduct({
-    idIngredient: `${meals.length + 1}`,
-    strIngredient: "Couillesdemoutons",
-    strDescription: "tout est dans son nom, mais c'est bon!",
-    price: 0
-});
-
-console.log("Après:" + meals.length);
-console.log(meals[574]);
 /** index.ts **/
 // import { PrismaClient } from "@prisma/client";
 // const prisma = new PrismaClient();
 
 async function main() {
     // Make changes here
+    console.log("et donc?");
 }
 
 main()
